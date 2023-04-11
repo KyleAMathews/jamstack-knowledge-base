b62065ba755813c249b31134d2aa2a1afaa1f037
Kyle Mathews <mathews.kyle@gmail.com>
2023-04-11 13:47:09 -0600

    Add basic gatsby site

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
